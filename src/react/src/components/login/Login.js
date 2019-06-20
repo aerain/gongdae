2309c374effbb26f70c936444c02f37ae4208e10
aerain <j40f893@gmail.com>
2019-06-21 03:21:04 +0900

    오타

--- src/react/src/components/login/Login.js
+++ src/react/src/components/login/Login.js
@@ -12,7 +12,7 @@ export default class Login extends Component {
                 />
                 <form method="POST" action="/login" className="login-form">
                     <input type="text" name="username" className="login-input" placeholder="아이디나 이메일을 입력하세요" />
-                    <inpsut type="password" name="password" className="login-input" placeholder="비밀번호를 입력하세요" />
+                    <input type="password" name="password" className="login-input" placeholder="비밀번호를 입력하세요" />
                     <input type="submit" value="로그인" className="login-submit"/>
                 </form>
             </div>
